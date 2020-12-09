67f1feb95df0bf5ceb7a295460d24a4f22f97cb3
Todd Jones <jonesgtodd@gmail.com>
2020-12-08 21:33:53 -0600

    added some code in  burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,14 @@
 let express = require("express");
 let router = express.Router();
 let burger = require("../models/burger.js");
+
+
+router.get("/", function(req, res) {
+	burger.selectAll(function(data) {
+		var hbsObject = {
+			burgers: data
+		};
+
+		res.render("index", hbsObject);
+	});
+});
