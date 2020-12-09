cb95365a1bc7c75d5c6eee23e32808868f525ffc
Todd Jones <jonesgtodd@gmail.com>
2020-12-08 22:14:49 -0600

    added db folder and schema and code

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Southern Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
